eb5a144e22224898c5e8c57a8db6684ec70b5c8f
pat <pat@MacBook-Air-de-pat.local>
2021-08-07 02:09:01 +0200

    Basic Scrolling Top Bar

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
     // Menu Set Up
-    
+
     var numSlides = $('#slider .slide').length;
 
     for (var i = 0; i < numSlides; i++) {
         
-        mifuncion(n);
+        
     }
 
     $(".container").scroll(function(event){
