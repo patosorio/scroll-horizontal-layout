ca79db38979e22490fbb76107d84b4830b6dad42
pat <pat@MacBook-Air-de-pat.local>
2021-08-07 04:43:06 +0200

    Scrolling Menu Version

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 $(document).ready(function() {
 
-    // Menu Set Up
-
     var numSlides = $('#slider .slide').length;
 
-    for (var i = 0; i < numSlides; i++) {
-        
-        
-    }
+    // Menu Set Up
+    for ( i = 0; i < numSlides; i++ ) {
+        var slideMenuButton = ("<div class='slideMenuButton'>" + "slideButton" + (i+1) + "</div>");
+        $("#scrolling-bar").append(slideMenuButton);
+    }    
+    
+    // Slide Menu Button width
+    
+    $(".slideMenuButton").width($(window).width()/numSlides);
+
+    $("#scroller").width($(".slideMenuButton").width());
+ 
+    var widthScroller = $("#scroller").width();
 
     $(".container").scroll(function(event){
+
         event.preventDefault();
-        var x = $(this).scrollLeft();
-        $("#scroller").width(x / (numSlides -1));
+
+        var horizontalScrolled = $(this).scrollLeft();
+
+        $("#scroller").width((horizontalScrolled / numSlides) + widthScroller);
     });
+
+
 });
+
 
 
 
